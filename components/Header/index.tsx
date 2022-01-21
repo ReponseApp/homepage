043e76b2a3ff2b78cc92580a18b4e3019a919d33
Mert Doğu <mertdoguonder@gmail.com>
2022-01-22 00:13:54 +0300

    change: style of og

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -11,22 +11,20 @@ export const Header:FC<IProps> = function({title}){
         <link rel="icon" href="assets/pinklogo.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-<meta name="title" content="Reponse App | Send messages, Not personal data."/>
-<meta name="description" content="Communication has never been this easy":>
+        <meta name="title" content="Reponse App | Send messages, Not personal data."/>
+        <meta name="description" content="Communication has never been this easy":>
 
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://reponse.app/">
-<meta property="og:title" content="Reponse App | Send messages, Not personal data."/>
-<meta property="og:description" content="Communication has never been this easy"/>
-<meta property="og:image" content="https://reponse.app/assets/pinklogo.png"/>
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://reponse.app/">
+        <meta property="og:title" content="Reponse App | Send messages, Not personal data."/>
+        <meta property="og:description" content="Communication has never been this easy"/>
+        <meta property="og:image" content="https://reponse.app/assets/pinklogo.png"/>
 
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image"/>
-<meta property="twitter:url" content="https://reponse.app/"/>
-<meta property="twitter:title" content="Reponse App | Send messages, Not personal data."/>
-<meta property="twitter:description" content="Communication has never been this easy"/>
-<meta property="twitter:image" content="https://reponse.app/assets/pinklogo.png"/>
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://reponse.app/"/>
+        <meta property="twitter:title" content="Reponse App | Send messages, Not personal data."/>
+        <meta property="twitter:description" content="Communication has never been this easy"/>
+        <meta property="twitter:image" content="https://reponse.app/assets/pinklogo.png"/>
 
       </Head>
     )
